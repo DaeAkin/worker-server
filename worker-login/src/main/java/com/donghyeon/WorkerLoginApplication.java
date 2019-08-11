d7544f1fd0f09bbc20269f99e40ea52db5fb7686
민동현 <mindonghyeon890@gmail.com>
2019-08-11 14:08:23 +0900

    add worker-login to Eureka

--- worker-login/src/main/java/com/donghyeon/WorkerLoginApplication.java
+++ worker-login/src/main/java/com/donghyeon/WorkerLoginApplication.java
@@ -2,6 +2,7 @@ package com.donghyeon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,7 +10,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableJpaAuditing
 @EnableJpaRepositories(repositoryFactoryBeanClass = org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFactoryBean.class)
 public class WorkerLoginApplication {
