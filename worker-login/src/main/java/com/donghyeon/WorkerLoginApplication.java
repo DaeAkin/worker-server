4fa0792fbccbd52da20e995006488b7134322f75
민동현 <mindonghyeon890@gmail.com>
2019-08-11 13:48:33 +0900

    initial commit

--- worker-login/src/main/java/com/donghyeon/WorkerLoginApplication.java
+++ worker-login/src/main/java/com/donghyeon/WorkerLoginApplication.java
@@ -2,12 +2,14 @@ package com.donghyeon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableJpaAuditing
 @EnableJpaRepositories(repositoryFactoryBeanClass = org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFactoryBean.class)
 public class WorkerLoginApplication {
