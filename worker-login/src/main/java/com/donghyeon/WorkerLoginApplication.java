5fac3fd311ff4286ff0bfc844f3991574659ae28
민동현 <mindonghyeon890@gmail.com>
2019-08-15 01:21:23 +0900

    back up

--- worker-login/src/main/java/com/donghyeon/WorkerLoginApplication.java
+++ worker-login/src/main/java/com/donghyeon/WorkerLoginApplication.java
@@ -3,6 +3,7 @@ package com.donghyeon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonAutoConfiguration;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
@@ -16,6 +17,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableJpaAuditing
 @EnableJpaRepositories(repositoryFactoryBeanClass = org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFactoryBean.class)
 @RibbonClient(name="worker-login", configuration = RibbonAutoConfiguration.class)
+@RefreshScope
 public class WorkerLoginApplication {
 
 
